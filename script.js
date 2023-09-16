4ec98430d197391645e8426e652651b62f2a1597
BWhittaker1415 <bwhittaker1415@gmail.com>
2023-09-15 20:44:06 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let coll = document.getElementsByClassName("collapsible");
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+// I had to reference "W3 schools" to find this answer.
